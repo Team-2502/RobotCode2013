660fce5d37b1fb27daf9c23106f8f003e7e7cf8e
Jackson <jjtcomkid@gmail.com>
2013-01-15 17:48:55 -0600

    Added base classes.

---
+++ src/com/team2502/robot2013/subsystems/Drivetrain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team2502.robot2013.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author Don
+ */
+public class Drivetrain extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
