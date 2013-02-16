3868fddaef44542554425acbb803343c67150b47
Josh-Larson <joshua-github@ourclan.net>
2013-02-15 18:39:29 -0600

    Fixed some bugs

--- src/com/team2502/robot2013/commands/storage/PushFrisbeeOut.java
+++ src/com/team2502/robot2013/commands/storage/PushFrisbeeOut.java
@@ -33,7 +33,7 @@ public class PushFrisbeeOut extends CommandBase {
 	
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return false;
+		return isTimedOut();
 	}
 	
 	// Called once after isFinished returns true
